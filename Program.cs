92bc7b16bc98a5d8c7cffbcf56d1b2ab9d9368c7
nadjitan <nadjitan@yahoo.com.ph>
2022-12-03 14:46:29 +0800

    add package

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ if (builder.Environment.IsDevelopment())
 else if (builder.Environment.IsProduction())
 {
     builder.Services.AddDbContext<TodoWebAppContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DATABASE_URL")));
+        options.UseMySQL(Environment.GetEnvironmentVariable("DATABASE_URL")!));
 }
 
 // Add services to the container.
