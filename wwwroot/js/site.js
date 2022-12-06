df9c66eb6018813c045e700af2620dcba2eca95e
nadjitan <nadjitan@yahoo.com.ph>
2022-12-06 09:50:06 +0800

    client side dates

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,14 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function getISOLocal(date = new Date()) {
+    const loc = Intl.DateTimeFormat().resolvedOptions().timeZone
+    return `${date.toLocaleDateString('en-CA', { timeZone: loc })}` +
+        `T${date.toLocaleTimeString('en', { timeZone: loc, hour12: false })}`
+}
+
+const createdDateInput = document.getElementById("add-created-date")
+
+if (createdDateInput) {
+    createdDateInput.setAttribute("value", getISOLocal())
+}
