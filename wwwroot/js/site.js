11204e95ffde7d5d1f13bca58aa4a1652920345f
nadjitan <nadjitan@yahoo.com.ph>
2022-12-07 16:31:40 +0800

    add animation

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,14 +1,12 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-function getISOLocal(date = new Date()) {
-    const loc = Intl.DateTimeFormat().resolvedOptions().timeZone
-    return `${date.toLocaleDateString('en-CA', { timeZone: loc })}` +
-        `T${date.toLocaleTimeString('en', { timeZone: loc, hour12: false })}`
+function getISOLocal() {
+    const date = new Date()
+    const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+    return `${date.toLocaleDateString('en-CA', { timeZone })}` +
+        `T${date.toLocaleTimeString('en', { timeZone, hour12: false })}`
 }
 
 const createdDateInput = document.getElementById("add-created-date")
-
-if (createdDateInput) {
-    createdDateInput.setAttribute("value", getISOLocal())
-}
+if (createdDateInput) createdDateInput.setAttribute("value", getISOLocal())
